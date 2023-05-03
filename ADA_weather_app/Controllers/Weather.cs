43098c7940909a34a5635d07644050f7af45389e
Tony <adaguayza@gmail.com>
2023-05-03 15:19:03 -0500

    first commit

---
+++ ADA_weather_app/Controllers/Weather.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADA_weather_app.Controllers
+{
+    public class Weather : Controller
+    {
+        public IActionResult Search()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Search(string searchTerm)
+        {
+            return View();
+        }
+    }
+}
